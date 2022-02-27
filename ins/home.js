275f418fcc6106f7a7f13e4184815c90d043b417
Hrithik <74535054+HRX101@users.noreply.github.com>
2022-02-27 20:48:20 +0530

    Add files via upload

---
+++ ins/home.js
@@ -0,0 +1,13 @@
+
+function reload(){
+    window.location.reload();
+}
+
+function show(){
+    alert("go down");
+    document.getElementById('show1').style.display='block';
+}
+function hide(){
+    
+    document.getElementById('show1').style.display="none";
+}
